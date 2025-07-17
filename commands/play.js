e7104eb046cdaddedd355a7acbaac62ac86156ca
AderexCantCode <arbot0175@gmail.com>
2025-07-17 10:37:44 +0700

    Pembaruan Bot

--- commands/play.js
+++ commands/play.js
@@ -9,10 +9,35 @@ module.exports = {
     const url = args[0];
     if (!url) return message.reply('❗ Kasih link YouTube/Spotify-nya!');
 
-    const stream = await playdl.stream(url);
-    const resource = createAudioResource(stream.stream, {
-      inputType: stream.type,
-    });
+    let stream, resource;
+    try {
+      if (playdl.is_expired()) await playdl.refreshToken();
+      if (playdl.yt_validate(url) === 'video') {
+        // YouTube
+        stream = await playdl.stream(url);
+        resource = createAudioResource(stream.stream, { inputType: stream.type });
+      } else if (playdl.sp_validate(url) === 'track') {
+        // Spotify track
+        const spotifyInfo = await playdl.spotify(url);
+        if (spotifyInfo && spotifyInfo.name && spotifyInfo.artists && spotifyInfo.artists.length > 0) {
+          // Search YouTube for the track
+          const search = await playdl.search(`${spotifyInfo.name} ${spotifyInfo.artists[0].name}`, { limit: 1 });
+          if (search && search.length > 0) {
+            stream = await playdl.stream(search[0].url);
+            resource = createAudioResource(stream.stream, { inputType: stream.type });
+          } else {
+            return message.reply('❗ Tidak dapat menemukan lagu di YouTube.');
+          }
+        } else {
+          return message.reply('❗ Tidak dapat mengambil info lagu dari Spotify.');
+        }
+      } else {
+        return message.reply('❗ Link tidak valid atau belum didukung.');
+      }
+    } catch (err) {
+      console.error(err);
+      return message.reply('❗ Terjadi kesalahan saat memutar lagu.');
+    }
 
     const connection = joinVoiceChannel({
       channelId: message.member.voice.channel.id,
